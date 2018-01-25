f9f2d796c1e062b880a0fe84d1a61c244c589ecf
Jj <jjdelc@gmail.com>
2018-01-25 04:54:41 -0500

    Factor out functions

--- main.js
+++ main.js
@@ -32,41 +32,72 @@ function micropubConfig(mpEndpoint, token) {
     return fetch(req).then(response => response.json());
 }
 
-function getAccessToken(url) {
-    const key = "accessToken:" + url;
-    const accessToken = localStorage.getItem(key);
-    return new Promise((resolve, reject) => {
-        if (!!accessToken) {
-            resolve(accessToken);
-        } else {
-            reject();
-        }
+
+const TokenManager = {
+    key: "accessToken:",
+    get: url => {
+        const key = TokenManager.key + url;
+        const accessToken = localStorage.getItem(key);
+        return new Promise((resolve, reject) => {
+            if (!!accessToken) {
+                resolve(accessToken);
+            } else {
+                reject();
+            }
+        });
+    },
+    store: (url, accessToken) => {
+        const key = TokenManager.key + url;
+        localStorage.setItem(key, accessToken);
+    }
+};
+
+
+function obtainToken(code, tokenEndpoint) {
+    const data = new FormData();
+    data.append('code', code);
+    data.append('client_id', "http://localhost:4321");
+    data.append('redirect_uri', "http://localhost:4321");
+    data.append('grant_type',"authorization_code");
+    const req = new Request(tokenEndpoint, {
+        method: 'POST',
+        body: data,
+        mode: 'cors'
+    });
+    return fetch(req).then(response => response.json()).then(r => {
+        return r.access_token
     });
 }
 
+
+function setUpMedia(mediaEndpoint, w) {
+    if (!!mediaEndpoint) {
+        console.log("Media endpoint supported");
+        w.mediaForm.style.display = 'block';
+        w.mediaForm.action = mediaEndpoint;
+        w.mediaForm.onsubmit = function(evt){
+            evt.preventDefault();
+            return false;
+        }
+    } else {
+        console.log("No media endpoint, resort to inline upload");
+        w.micropubForm.enctype = "multipart/form-data";
+        w.inlineMediaField.style.display = 'inline-block';
+    }
+}
+
 function startEditor(siteUrl, token, w) {
-    w.authScreen.style.display = 'none';
-    w.postScreen.style.display = 'block';
+    w.authSection.style.display = 'none';
+    w.postSection.style.display = 'block';
+    w.tokenField.value = token;
     discoverLink(siteUrl, "micropub").then(mpUrl => {
-        w.tokenField.value = token;
         w.micropubForm.action = mpUrl;
-        micropubConfig(mpUrl, token).then(config => {
-            const mediaEndpoint = config['media-endpoint'];
-            if (!!mediaEndpoint) {
-                console.log("Media endpoint supported");
-                w.mediaForm.style.display = 'block';
-                w.mediaForm.action = mediaEndpoint;
-                w.mediaForm.onsubmit = function(evt){
-                    evt.preventDefault();
-                    return false;
-                }
-            } else {
-                console.log("No media endpoint, resort to inline upload");
-                w.micropubForm.enctype = "multipart/form-data";
-                w.inlineMediaField.style.display = 'inline-block';
-            }
-        })
-    });
+        return mpUrl
+    }).then(
+        mpUrl => micropubConfig(mpUrl, token)
+    ).then(config => {
+        setUpMedia(config['media-endpoint'], w);
+    })
 }
 
 function startAuthDance(siteUrl, w) {
@@ -76,43 +107,41 @@ function startAuthDance(siteUrl, w) {
     })
 }
 
+function editorScreen(me, code, w) {
+    discoverLink(me, "token_endpoint").then(
+        tokenEndpoint => obtainToken(code, tokenEndpoint)
+    ).then(accessToken => {
+        TokenManager.store(me, accessToken);
+        startEditor(me, accessToken, w);
+    }, error => {
+        alert(error);
+        w.postSection.style.display = 'none';
+    });
+}
+
+
+function authScreen(w) {
+    w.authForm.onsubmit = function(evt){
+        evt.preventDefault();
+        const siteUrl = w.indie_auth_url.value;
+        TokenManager.get(siteUrl).then(
+            token => startEditor(siteUrl, token, w)
+        ).catch(
+            () => startAuthDance(siteUrl, w)
+        );
+        return false;
+    }
+}
+
+
 (function(w){
     const params = new URLSearchParams(w.location.search);
     const code = params.get('code');
     if (!!code) {
-        w.authScreen.style.display = 'none';
-        const data = new FormData();
-        data.append('code', code);
-        data.append('client_id', "http://localhost:4321");
-        data.append('redirect_uri', "http://localhost:4321");
-        data.append('grant_type',"authorization_code");
-
-        discoverLink(params.get('me'), "token_endpoint").then(tokenEndpoint => {
-            const req = new Request(tokenEndpoint, {
-              method: 'POST',
-              body: data,
-              mode: 'cors'
-            });
-            fetch(req).then(response => response.json()).then(r => {
-                const key = "accessToken:" + r.me;
-                localStorage.setItem(key, r.access_token);
-                startEditor(r.me, r.access_token, w);
-            }, error => {
-                alert(error);
-                w.postScreen.style.display = 'none';
-            });
-        });
+        w.authSection.style.display = 'none';
+        editorScreen(params.get('me'), code, w);
     } else {
-        w.authForm.onsubmit = function(evt){
-            evt.preventDefault();
-            const siteUrl = w.indie_auth_url.value;
-            getAccessToken(siteUrl).then(
-                token => startEditor(siteUrl, token, w)
-            ).catch(
-                err => startAuthDance(siteUrl, w)
-            );
-            return false;
-        }
+        authScreen(w);
     }
 
     w.resetApp.onclick = () => localStorage.clear()
